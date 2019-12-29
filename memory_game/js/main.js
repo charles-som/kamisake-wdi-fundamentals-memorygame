6f319d7d132a7dc6d23ccd0f2d28687662fa1df4
Chaz <chaz@Chaz.local>
2019-12-29 12:39:48 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+},
+];
+
+var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -10,34 +32,22 @@ function checkForMatch() {
 } 
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-    if (cardsInPlay.length ===2) {
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+    if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry try again.");
+    };
     checkForMatch();
 }
 }
 
+
 flipCard(0);
 flipCard(2);
-
-/* if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!");
-  } else { alert("Sorry try again.");
-}
-} */
-
-
-
-/* First you'll create a function that will store all steps related to selecting, or flipping over, a card. 
-When the user flips a card over, you'll want to add that card to the array of cards that are in play.
-If the user has flipped over two cards, you'll want to check for a match.
-
-/* Second, you'll create a function to group together the logic to check to see if the two cards that 
-the user has flipped over match each other. You'll provide feedback to the user letting them know if 
-the two cards match, or if they should try again. */
-
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardTwo);
 
 
