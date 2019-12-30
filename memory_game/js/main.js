0588ff0404fbda30a3cc3269510464b70cfde6ed
Chaz <chaz@Chaz.local>
2019-12-30 10:44:10 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,31 +23,31 @@ cardImage: "images/king-of-diamonds.png"
 
 var cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+        };
 }
-} 
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry try again.");
-    };
-    checkForMatch();
+    }
 }
 }
 
 
-flipCard(0);
-flipCard(2);
-
-
+createBoard();
